4fe6eb28467e603d69c070c805fa2b6157f11a4a
ASOST\1601297 <1601297@st.asojuku.ac.jp>
2018-09-13 14:04:45 +0900

    no message

---
+++ ヤノ/EnemyMave.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMave : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        Walk,
+        Wait,
+        Chase
+    };
+
+    //　敵の状態
+    private EnemyState state;
+    //　追いかけるキャラクター
+    private Transform playerTransform;
+
+    Rigidbody _rb;
+    public bool isUseCameraDirection;    // カメラの向きに合わせて移動させたい場合はtrue
+    public float moveSpeed;              // 移動速度
+    public float moveForceMultiplier;    // 移動速度の入力に対する追従度
+    public GameObject mainCamera;
+    float _horizontalInput;
+    float _verticalInput;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        state = EnemyState.Walk;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        //　キャラクターを追いかける状態であればキャラクターの目的地を再設定
+        if (state == EnemyState.Chase)
+        {
+            if(playerTransform.position.x < transform.position.x)
+            {
+                _horizontalInput=(_horizontalInput < -1.0f ?  -1.0f : _horizontalInput -=(0.1f));
+            }
+        }
+    }
+
+    public void SetState(string mode, Transform obj = null)
+    {
+        if (mode == "walk")
+        {
+            //arrived = false;
+            //elapsedTime = 0f;
+            //state = EnemyState.Walk;
+            //setPosition.CreateRandomPosition();
+        }
+        else if (mode == "chase")
+        {
+            state = EnemyState.Chase;
+            //　追いかける対象をセット
+            playerTransform = obj;
+        }
+        else if (mode == "wait")
+        {
+ 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveVector = Vector3.zero;    // 移動速度の入力
+
+        if (isUseCameraDirection)
+        {
+            Vector3 cameraForward = mainCamera.transform.forward;
+            Vector3 cameraRight = mainCamera.transform.right;
+            cameraForward.y = 0.0f;    // 水平方向に移動させたい場合はy方向成分を0にする
+            cameraRight.y = 0.0f;
+
+            moveVector = moveSpeed * (cameraRight.normalized * _horizontalInput + cameraForward.normalized * _verticalInput);
+        }
+        else
+        {
+            moveVector.x = moveSpeed * _horizontalInput;
+            moveVector.z = moveSpeed * _verticalInput;
+        }
+
+        _rb.AddForce(moveForceMultiplier * (moveVector - _rb.velocity));
+    }
+
+    public EnemyState GetState()
+    {
+        return state;
+    }
+}
---
+++ ヤノ/YukaMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YukaMove : MonoBehaviour {
+
+    public int time = 100;
+    public string PathName = "Kari";
+
+    // Use this for initialization
+    void Start()
+    {
+        //iTween.MoveTo(this.gameObject, iTween.Hash("path", iTweenPath.GetPath(PathName), "time", time, "easeType", iTween.EaseType.linear, "looptype", iTween.LoopType.pingPong));
+        iTween.MoveTo(this.gameObject, iTween.Hash("path", iTweenPath.GetPath(PathName), "time", time, "easeType", iTween.EaseType.linear, "looptype", iTween.LoopType.pingPong));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.SetParent(transform);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    { 
+        if (other.tag == "Player")
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
