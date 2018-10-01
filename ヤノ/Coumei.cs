dd5399b295d692452f9438a26fb833311ab22ebf
ASOST\1601297 <1601297@st.asojuku.ac.jp>
2018-10-01 15:16:31 +0900

    今です！

---
+++ ヤノ/Coumei.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coumei : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+
+    private Color defColor;
+
+    // Use this for initialization
+    void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        defColor = sprite.color;
+        sprite.color=new Color(0f,0f,0f,0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            sprite.color = defColor;
+        }
+    }
+}
