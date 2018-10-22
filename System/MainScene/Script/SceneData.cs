5738dc20a0b75072b0ca455743cb471423a2f8f7
ASOST\1601297 <1601297@st.asojuku.ac.jp>
2018-10-22 11:51:20 +0900

    no message

--- System/MainScene/Script/SceneData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-
-public class SceneData : MonoBehaviour {
-    //　シーン管理スクリプト
-    private readonly LoadSceneManager loadSceneManager;
-}
