e3f5a07814d234004dd9d3d5a819f34a2ce34eff
ASOST\1601297 <1601297@st.asojuku.ac.jp>
2018-10-22 11:53:09 +0900

    Revert "no message"
    This reverts commit 5738dc20a0b75072b0ca455743cb471423a2f8f7.

---
+++ System/MainScene/Script/LoadSceneButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour {
+
+    private LoadSceneManager loadSceneManager;
+    public string sceneName;
+
+    // Use this for initialization
+    void Start () {
+        loadSceneManager = GameObject.Find("Management").GetComponent<LoadSceneManager>();
+    }
+
+    // Update is called once per frame
+    public void ClickButton()
+    {
+        loadSceneManager.FadeAndLoadScene(sceneName);
+    }
+}
