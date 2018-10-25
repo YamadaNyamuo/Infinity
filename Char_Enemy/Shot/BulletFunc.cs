8b0fbc86ec9dbe7fac8dd4ed2b59b9ac46474b60
ASOST\1601336 <1601336@st.asojuku.ac.jp>
2018-10-25 12:26:03 +0900

    敵のショットの追加

---
+++ Char_Enemy/Shot/BulletFunc.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFunc : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector3 PPos;
+
+    //これ（弾）自体の位置座標
+    Vector3 Position;
+    //弾の速さ
+    public float SPEED;
+    //プレイヤーの情報　これの発射元から取ってきてる
+    GameObject Player;
+
+    //プレイヤーと飛んでる敵の間の距離をｘ軸とy軸に分けてある
+    float tX;
+    float tY;
+    float alfa;
+    Vector3 angle;
+
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.Find("Player");
+        Position = transform.position;
+        //スピードはプレイヤー参照して変える
+        SPEED = 6.0f;
+        angle = (PPos - transform.position);
+        angle = angle / angle.magnitude;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //画面外に出たらこのオブジェクトを消す
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(angle * SPEED * Time.deltaTime, Space.World);
+    }
+
+    public void Fire(Vector3 pos, Vector3 pPos)     //posはこの弾自体の位置 pPosはプレイヤーの位置
+    {
+        PPos = pPos;
+        transform.position = pos;
+    }
+}
---
+++ Char_Enemy/Shot/ShotFunc.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotFunc : MonoBehaviour {
+
+    //敵の攻撃フラグ
+    public bool EAtackFlag;
+
+    //プレイヤーの情報を取得
+    public GameObject Player;
+    public GameObject Bullet;
+
+    Vector2 PlayerPos;
+    Vector3 offsetpos;
+
+    enum DIR{
+        RIGHT,
+        LEFT
+    };
+    DIR dir;
+
+    // Use this for initialization
+    void Start () {
+        EAtackFlag = false;
+        //Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetComponent<Renderer>().isVisible)
+        {
+            AI();
+            if (!GameObject.Find("Bullet(Clone)"))
+            {
+                EAtackFlag = true;
+            }
+        }
+        else
+        {
+            EAtackFlag = false;
+        }
+        
+    }
+
+    void AI()
+    {
+        if (transform.position.x >= Player.transform.position.x)
+        {
+            dir = DIR.LEFT;
+            offsetpos = -(transform.localScale / 2);
+        }
+        else
+        {
+            dir = DIR.RIGHT;
+            offsetpos = (transform.localScale / 2);
+        }
+
+        if (EAtackFlag == true)
+        {
+            //一旦新しいオブジェクトを作る
+            GameObject bulletClone = Instantiate(Bullet, transform.position, transform.rotation);
+            BulletFunc ActBeam = bulletClone.GetComponent<BulletFunc>();
+            ActBeam.Fire(transform.position ,Player.transform.position);
+            EAtackFlag = false;
+        }
+    }
+}
